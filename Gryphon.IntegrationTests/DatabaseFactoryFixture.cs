ae41cc14b0c5b064da2fc1d92022f31087ffa606
Egor Pankov <pankov.egor26032005@yandex.ru>
2025-01-23 13:57:26 +0300

    feat: added database factory fixture

---
+++ Gryphon.IntegrationTests/DatabaseFactoryFixture.cs
@@ -0,0 +1,8 @@
+
+namespace Gryphon.IntegrationTests;
+
+internal class DatabaseFactoryFixture : IAsyncLifetime
+{
+    public Task DisposeAsync() => throw new NotImplementedException();
+    public Task InitializeAsync() => throw new NotImplementedException();
+}
